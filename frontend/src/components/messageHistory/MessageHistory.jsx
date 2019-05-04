7c065d1fde8bfa8a226b33a7f890411fb8485aff
Cristian  Chaparro <cristianchaparroa@Cristians-MacBook-Pro.local>
2019-05-04 13:07:37 -0500

    Reder just the 50 first  messages

--- frontend/src/components/messageHistory/MessageHistory.jsx
+++ frontend/src/components/messageHistory/MessageHistory.jsx
@@ -5,8 +5,10 @@ import Message from "../message/Message";
 class MessageHistory extends Component {
 
   render () {
-    const messages = this.props.messages.map( (message, index)  => <Message key={message.id} message={message} />).reverse();
 
+    const messages = this.props.messages.map(
+      message => <Message key={message.id} message={message} />
+    ).reverse().filter((message,idx) => idx < 50);
 
     return (
       <div className="message-history">
