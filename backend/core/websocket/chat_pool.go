ce01227ba354fb31cac557c9b0b7864c1fc0419f
Cristian  Chaparro <cristianchaparroa@Cristians-MacBook-Pro.local>
2019-05-12 10:25:00 -0500

    Decouple with interfaces the client in the chat pool

--- backend/core/websocket/chat_pool.go
+++ backend/core/websocket/chat_pool.go
@@ -4,20 +4,40 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/cristianchaparroa/humanity/backend/services"
 	"github.com/google/uuid"
 )
 
+// IChatPool defines the methods to use in  the  pool of connections for a chat
+// over one room.
+type IChatPool interface {
+
+	// Start liste all events in the pool of connections
+	Start()
+
+	// Register a client in the pool
+	RegisterClient(client IClient)
+
+	// Unregister a client in the pool
+	UnregisterClient(client IClient)
+
+	// Broadcast send a message to all clients in the pool
+	BroadcastMessage(message Message)
+}
+
 // Pool manage the concurrent comunication
 type Pool struct {
+
 	// Register channel send to all clients the message
 	// that a new client is joined
-	Register chan *Client
+	Register chan IClient
+
 	// Unregister channel unregister a user and notify the pool
 	// when a client is disconnected.
-	Unregister chan *Client
+	Unregister chan IClient
+
 	// Cients that are active/inactiv but not disconnected.
-	Clients map[*Client]bool
+	Clients map[IClient]bool
+
 	// Broadcast send a message to all client in the pool
 	Broadcast chan Message
 }
@@ -25,9 +45,9 @@ type Pool struct {
 // NewPool creates a pointer to Pool structure
 func NewPool() *Pool {
 	return &Pool{
-		Register:   make(chan *Client),
-		Unregister: make(chan *Client),
-		Clients:    make(map[*Client]bool),
+		Register:   make(chan IClient),
+		Unregister: make(chan IClient),
+		Clients:    make(map[IClient]bool),
 		Broadcast:  make(chan Message),
 	}
 }
@@ -38,88 +58,70 @@ func (p *Pool) Start() {
 
 		select {
 		case client := <-p.Register:
-			RegisterClient(p, client)
+			p.RegisterClient(client)
 			break
 
 		case client := <-p.Unregister:
-			UnregisterClient(p, client)
+			p.UnregisterClient(client)
 			break
 
 		case message := <-p.Broadcast:
-			BroadcastMessage(p, message)
+			p.BroadcastMessage(message)
 			break
-
 		}
-
 	}
 }
 
 // RegisterClient ...
-func RegisterClient(p *Pool, client *Client) {
+func (p *Pool) RegisterClient(client IClient) {
 
 	fmt.Println("--> RegisterClient")
 
 	p.Clients[client] = true
+
 	for c := range p.Clients {
-		uuid := uuid.New()
-		fmt.Println(client)
-		m := Message{ID: uuid.String(), Type: 1, Body: fmt.Sprintf(" %s joined... ", client.Account.Nickname), Time: time.Now()}
-		c.Conn.WriteJSON(m)
+		body := fmt.Sprintf(" %s joined... ", "")
+		m := NewMessage(body, 1)
+		conn := c.GetConnection()
+		conn.WriteJSON(m)
 	}
 
 	fmt.Println("<-- RegisterClient")
 }
 
 // UnregisterClient ...
-func UnregisterClient(p *Pool, client *Client) {
+func (p *Pool) UnregisterClient(client IClient) {
 
 	fmt.Println("--> UnregisterClient")
 
 	delete(p.Clients, client)
 	for c := range p.Clients {
-		uuid := uuid.New()
-		m := Message{ID: uuid.String(), Type: 1, Body: "User Disconnected", Time: time.Now()}
-		c.Conn.WriteJSON(m)
+		body := "User Disconnected"
+		m := NewMessage(body, 1)
+		conn := c.GetConnection()
+		conn.WriteJSON(m)
 	}
 
 	fmt.Println("<-- UnregisterClient")
 }
 
 // BroadcastMessage ...
-func BroadcastMessage(p *Pool, message Message) {
+func (p *Pool) BroadcastMessage(m Message) {
 
 	fmt.Println("--> BroadcastMessage Sending message to all clients in this pool")
 
-	ms := make([]Message, 0)
-	ms = append(ms, message)
-
-	text := message.Body
-	bs := services.NewBotService()
-	intent := bs.GetIntent(text)
-
-	if intent == services.StockIntent {
-		im := bs.Process(text)
-
-		m := Message{
-			Body:     im.RawMessage,
-			Nickname: "Bot",
-		}
-		ms = append(ms, m)
-	}
-
 	for c := range p.Clients {
 
-		for _, m := range ms {
+		uuid := uuid.New()
+		m.Time = time.Now()
+		m.ID = uuid.String()
 
-			uuid := uuid.New()
-			m.Time = time.Now()
-			m.ID = uuid.String()
+		fmt.Printf("--> message to send: \n--> %#v \n", m)
+		conn := c.GetConnection()
 
-			fmt.Printf("--> message to send: \n--> %#v \n", m)
-			if err := c.Conn.WriteJSON(m); err != nil {
-				fmt.Println(err)
-				return
-			}
+		if err := conn.WriteJSON(m); err != nil {
+			fmt.Println(err)
+			return
 		}
 	}
 
--- backend/core/websocket/client.go
+++ backend/core/websocket/client.go
@@ -2,45 +2,72 @@ package websocket
 
 import (
 	"log"
-	"time"
 
 	"github.com/cristianchaparroa/humanity/backend/models"
 	"github.com/gorilla/websocket"
 )
 
+// IClient defines the methods that sould have a client
+// in the chat pool
+type IClient interface {
+
+	// GetID retrieves the client id
+	GetID() string
+
+	// GetPool returns the belongs pool
+	// TODO : decouple the pool an replace by IChatPool
+	GetPool() *Pool
+
+	//  GetConnection retrieves the websocket connection
+	GetConnection() *websocket.Conn
+
+	// Read the message int the pool
+	Read()
+}
+
 // Client specific to websocket connection.
 type Client struct {
+
 	// ID is the string for specific connection
 	ID string
+
 	// Conn is  pointer to websocket.Conn
 	Conn *websocket.Conn
+
 	// Pool Pointer to the pool which this client will be part
 	Pool *Pool
 
+	// Account information related to this client
 	Account *models.Account
 }
 
-// Message contains all the information througth a connection
-type Message struct {
-	ID       string    `json:"id"`
-	Type     int       `json:"type"`
-	Body     string    `json:"body"`
-	UserID   string    `json:"user_id"`
-	Nickname string    `json:"nickname"`
-	Time     time.Time `json:"time"`
+// GetID ...
+func (c *Client) GetID() string {
+	return c.ID
 }
 
+// GetPool ...
+func (c *Client) GetPool() *Pool {
+	return c.Pool
+}
+
+// GetConnection ...
+func (c *Client) GetConnection() *websocket.Conn {
+	return c.Conn
+}
+
+// Read the message in the current connection
 func (c *Client) Read() {
+
 	defer func() {
-		c.Pool.Unregister <- c
+		//pool := c.GetPool()
+		//c.Pool.Unregister <- c
 		c.Conn.Close()
 	}()
 
 	for {
 		var message Message
-		// messageType, p, err := c.Conn.ReadMessage()
 		err := c.Conn.ReadJSON(&message)
-
 		message.Type = 1
 
 		if err != nil {
